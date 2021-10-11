99288b31a971a434c5d032ea0a100e5da4765c80
MinukLee1 <www77mm@naver.com>
2021-10-11 21:09:29 +0900

    연습문제

---
+++ Codeflow/연습문제.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	//별찍기 
+	//input을 주면 그에 맞는 N*N 출력
+	int input; 
+	cin >> input; 
+
+	for (int j = 0; j < input; j++) {
+		for (int i = 0; i < input; i++) {
+			cout << "*";
+		}
+		cout << endl;
+	}
+
+	//구구단
+	for (int k = 2; k <= 9; k++) {
+		for (int g = 1; g <= 9; g++) {
+			cout << k << "*" << g << "=" << k * g << endl;
+		}
+	}
+
+}
+
