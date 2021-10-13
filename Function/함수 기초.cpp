e90adfcc36846137922874b27dc40d46c4047c0c
MinukLee1 <www77mm@naver.com>
2021-10-13 23:29:17 +0900

    함수 기초

---
+++ Function/함수 기초.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std; 
+
+//함수 (프로시저, 메소드, 루틴) 
+
+/*
+input으로 무엇을 받고, output으로 무엇을 뱉을지 정해준다 
+
+반환타입 함수이름([인자타입 매개변수]){
+내용 ~ 
+return~;
+*/
+void PrintNumber(int number) {
+
+	cout << "넘겨주신 숫자는" << number << "입니다" << endl;
+}
+
+int MultiplyBy2(int a) {
+
+	int b = a * 2; 
+	return b; 
+}
+
+int main()
+{
+	int result = MultiplyBy2(3);
+
+	PrintNumber(result);
+	//PrintHelloWorld();
+	return 0; 
+}
