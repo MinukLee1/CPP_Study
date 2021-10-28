b88fa7791a3b3822badae88034497efd8c020aac
MinukLee1 <www77mm@naver.com>
2021-10-28 13:40:48 +0900

    호출 스택

---
+++ Function/호출 스택.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+using namespace std; 
+
+//호출 스택 
+
+//함수 선언 
+void Func1();
+void Func2(int a, int b);
+void Func3(float a);
+
+void Func1() {
+    cout << "Func1" << endl;
+
+    // push 2
+    // push 1 
+    // call Func2 : 주소값 호출 
+    Func2(1, 2);
+}
+
+void Func2(int a, int b) {
+    cout << "Func2" << endl;
+
+
+    Func3(10);
+}
+
+void Func3(float a) {
+    cout << "Func3" << endl;
+}
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    Func1();
+}
